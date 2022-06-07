7dc2f24a2e84e63be55f0587f565ccd8d6b8fc5f
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2022-06-07 17:51:15 +0100

    added version whitelist

--- deepcore/index.php
+++ deepcore/index.php
@@ -1,11 +1,11 @@
 <?php
 $rootDIR =  dirname(__DIR__) . "/";
 require_once($rootDIR . "routes.php");
+require_once($rootDIR . "versions.php");
 $method = htmlspecialchars($_SERVER['REQUEST_METHOD'],  ENT_QUOTES, 'UTF-8');
 $data = json_decode(file_get_contents('php://input'), true);
 $routing = htmlspecialchars($data["routing"],  ENT_QUOTES, 'UTF-8');
-$v1 = explode("-", htmlspecialchars($data["version"],  ENT_QUOTES, 'UTF-8'));
-$version = implode(".", $v1);
+$version = htmlspecialchars($data["version"],  ENT_QUOTES, 'UTF-8');
 
 
 if (array_key_exists($routing, $allowedRoutes)) {
@@ -13,9 +13,22 @@ if (array_key_exists($routing, $allowedRoutes)) {
 } else {
     header("Content-Type: application/json");
     http_response_code(405);
-    echo "ERROR -> No Available Route Found -> " . $routing;
+    $errorArray[0]['Error'] = "No Available Route Found -> " . $routing;
+    echo json_encode($errorArray);
     exit();
 }
+
+if (array_key_exists($version, $allowedVersion)) {
+    $route .= $allowedVersion[$version] . "/";
+} else {
+    header("Content-Type: application/json");
+    http_response_code(405);
+    $errorArray[0]['Error'] = "No Available Version Found -> " . $version;
+    echo json_encode($errorArray);
+    exit();
+}
+
+
 require_once($rootDIR . "deepstore/meekro.php");
 if (!empty(htmlspecialchars($data["help"],  ENT_QUOTES, 'UTF-8'))) {
     $helpPath = $route . "help.php";
@@ -33,31 +46,31 @@ if (empty($route) && empty($version)) {
 require_once($rootDIR . "/deepcore/api.php");
 switch ($method) {
     case "PUT":
-        $servicePath = $route . "/" . $version . "/put.php";
+        $servicePath = $route . "put.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "POST":
-        $servicePath = $route . "/" . $version . "/post.php";
+        $servicePath = $route . "post.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "PATCH":
-        $servicePath = $route . "/" . $version . "/patch.php";
+        $servicePath = $route . "patch.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "GET":
-        $servicePath = $route . "/" . $version . "/get.php";
+        $servicePath = $route . "get.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "DELETE":
-        $servicePath = $route . "/" . $version . "/delete.php";
+        $servicePath = $route . "delete.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
