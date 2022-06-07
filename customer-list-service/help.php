9df9583ee66fce45c70c9c2fcbadc4eb3ea28dd2
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2022-06-07 17:34:33 +0100

    new service path build

--- customer-list-service/help.php
+++ customer-list-service/help.php
@@ -4,11 +4,21 @@ echo "Version Number: 1.0.0" . PHP_EOL;
 echo "The following is the payload format (All fields are required): " . PHP_EOL . PHP_EOL;
 echo '{
         "routing": "{Service Routing}",
-        "version": "{version.version.version}",
+        "version": "{major-minor-patch}",
         "api-key": "{API-KEY}",
         "client_id": "{Client ID}",
         "get_from": "{first || last}",
         "condition": "{match || equal}"
         "search_for": "{full or partial string}"
     }' . PHP_EOL . PHP_EOL;
+echo "Example:" . PHP_EOL . PHP_EOL;
+echo '{
+    "routing": "CustomerListService",
+    "version": "1-0-0",
+    "api-key": "ABC123",
+    "client_id": "SUPERCLIENT",
+    "get_from": "last",
+    "condition": "equal"
+    "search_for": "smith"
+}' . PHP_EOL . PHP_EOL;
 exit();
--- deepcore/index.php
+++ deepcore/index.php
@@ -1,11 +1,10 @@
 <?php
 $rootDIR =  dirname(__DIR__) . "/";
 require_once($rootDIR . "routes.php");
-$method = $_SERVER['REQUEST_METHOD'];
-$method = htmlspecialchars($method,  ENT_QUOTES, 'UTF-8');
+$method = htmlspecialchars($_SERVER['REQUEST_METHOD'],  ENT_QUOTES, 'UTF-8');
 $data = json_decode(file_get_contents('php://input'), true);
 $routing = htmlspecialchars($data["routing"],  ENT_QUOTES, 'UTF-8');
-$v1 = explode(".", $data['version']);
+$v1 = explode("-", $data['version']);
 $version = implode(".", $v1);
 
 
@@ -23,38 +22,42 @@ if (!empty(htmlspecialchars($data["help"],  ENT_QUOTES, 'UTF-8'))) {
     require_once($helpPath);
     exit();
 }
-if (!empty($routing) && !empty($version)) {
-    $servicePath = $route . "/" . $version . "/" . strtolower($method) . ".php";
-} else {
+if (empty($route) && empty($version)) {
     header("Content-Type: application/json");
     http_response_code(400);
     $errorArray[0]['Error'] = 'No Route and / or Version given';
     echo json_encode($errorArray);
     exit();
 }
+
 require_once($rootDIR . "/deepcore/api.php");
 switch ($method) {
     case "PUT":
+        $servicePath = $route . "/" . $version . "/put.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "POST":
+        $servicePath = $route . "/" . $version . "/post.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "PATCH":
+        $servicePath = $route . "/" . $version . "/patch.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "GET":
+        $servicePath = $route . "/" . $version . "/get.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
         break;
     case "DELETE":
+        $servicePath = $route . "/" . $version . "/delete.php";
         if (!file_exists($servicePath)) {
             routeError($method, $version);
         }
