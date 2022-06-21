b9ca3076f7ec5c2c2e4cbd0da7e2901c396b661f
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2022-06-21 09:59:26 +0100

    Removed comments

--- login-service/1.0.1/get.php
+++ login-service/1.0.1/get.php
@@ -1,16 +1,5 @@
 <?php
 error_reporting(E_ALL);
-
-/*TODO Error check the data
-This endpoint requires the username and password, 
-so we need to check if they have been supplied.
-If they are not both present and have values, then
-return an error, along with an explaination
-
-carolina.lima 
-17ecb08a7049bd62
-*/
-
 if (!isset($data['username'], $data['password'])) {
     header("Content-Type: application/json");
     http_response_code(400);
