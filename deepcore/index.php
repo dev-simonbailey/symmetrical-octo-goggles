9a76c19a7357f5c054765e18f481cb3090f023ee
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2022-07-04 16:26:46 +0100

    cleaned the main index file

--- deepcore/index.php
+++ deepcore/index.php
@@ -1,94 +1,119 @@
 <?php
+
+/**
+ * Set the rootDIR
+ */
 $rootDIR =  dirname(__DIR__);
+/**
+ * Require the routes whitelist
+ */
 require_once($rootDIR . "/routes.php");
+/**
+ * Require the version whitelist
+ */
 require_once($rootDIR . "/versions.php");
+/**
+ * Cleanse the REQUEST_METHOD
+ */
 $method = htmlspecialchars($_SERVER['REQUEST_METHOD'],  ENT_QUOTES, 'UTF-8');
+/**
+ * Decode the JSON data as associative array
+ */
 $data = json_decode(file_get_contents('php://input'), true);
-$routing = htmlspecialchars($data["routing"],  ENT_QUOTES, 'UTF-8');
+/**
+ * Cleanse the requested route string
+ */
+$route = htmlspecialchars($data["route"],  ENT_QUOTES, 'UTF-8');
+/**
+ * Cleanse the requested action string
+ */
+$action = htmlspecialchars($data["action"],  ENT_QUOTES, 'UTF-8');
+/**
+ * Cleanse the requested version string
+ */
 $version = htmlspecialchars($data["version"],  ENT_QUOTES, 'UTF-8');
-if (array_key_exists($routing, $allowedRoutes)) {
-    $route = $rootDIR . "/" . $allowedRoutes[$routing] . "/";
+/**
+ * If the requested resource route is allowed, then build routePath, otherwise error
+ */
+if (array_key_exists($route, $allowedRoutes)) {
+    $routePath = $rootDIR . "/" . $allowedRoutes[$route] . "/" . $action . "/";
 } else {
     header("Content-Type: application/json");
-    http_response_code(405);
-    $errorArray[0]['Error'] = "No Available Route Found -> " . $routing;
-    echo json_encode($errorArray);
-    exit();
+    errorHandler(405, "No Available Route Found -> " . $route);
 }
+/**
+ * If the requested resource version is allowed, then add to routePath, otherwise error
+ */
 if (array_key_exists($version, $allowedVersions)) {
-    $route .= $allowedVersions[$version] . "/";
+    $routePath .= $allowedVersions[$version] . "/";
 } else {
     header("Content-Type: application/json");
-    http_response_code(405);
-    $errorArray[0]['Error'] = "No Available Version Found -> " . $version;
-    echo json_encode($errorArray);
-    exit();
+    errorHandler(405, "No Available Version Found -> " . $version);
 }
+/**
+ * If the requested help file does not exist, error
+ */
 if (isset($data["help"])) {
-    $helpPath = $route . "help.php";
+    $helpPath = $routePath . "help.php";
     if (!file_exists($helpPath)) {
-        header("Content-Type: application/json");
-        http_response_code(405);
-        $errorArray[0]['Error'] = 'HELP is not available on this route with version number: ' . $version;
-        echo json_encode($errorArray);
-        exit();
+        errorHandler(405, 'HELP is not available on this route with version number: ' . $version);
     }
     require_once($helpPath);
     exit();
 }
+/**
+ * Require the meekroDB class
+ */
 require_once($rootDIR . "/deepstore/meekro.php");
-if (empty($route) && empty($version)) {
-    header("Content-Type: application/json");
-    http_response_code(400);
-    $errorArray[0]['Error'] = 'No Route and / or Version given';
-    echo json_encode($errorArray);
-    exit();
+if (empty($routePath) && empty($version)) {
+    errorHandler(400, "No Route and / or Version given");
 }
+/**
+ * Require the api check
+ */
 require_once($rootDIR . "/deepcore/api.php");
-switch ($method) {
-    case "PUT":
-        $servicePath = $route . "put.php";
-        if (!file_exists($servicePath)) {
-            routeError($method, $version);
-        }
-        break;
-    case "POST":
-        $servicePath = $route . "post.php";
-        if (!file_exists($servicePath)) {
-            routeError($method, $version);
-        }
-        break;
-    case "PATCH":
-        $servicePath = $route . "patch.php";
-        if (!file_exists($servicePath)) {
-            routeError($method, $version);
-        }
-        break;
-    case "GET":
-        $servicePath = $route . "get.php";
-        if (!file_exists($servicePath)) {
-            routeError($method, $version);
-        }
-        break;
-    case "DELETE":
-        $servicePath = $route . "delete.php";
-        if (!file_exists($servicePath)) {
-            routeError($method, $version);
-        }
-        break;
-    default:
-        header("Content-Type: application/json");
-        http_response_code(405);
-        echo "ERROR -> No Available Method Found -> " . $routing;
-        exit();
-        break;
+/**
+ * Add the default filename to the routePath
+ */
+$routePath .= "index.php";
+/**
+ * If the requested resource doesn't exist, error
+ */
+if (!file_exists($routePath)) {
+    routeError($route, $action, $version);
 }
-require_once($servicePath);
-function routeError($method, $version)
+/**
+ * require the requested resource
+ */
+require_once($routePath);
+/**
+ * routeError
+ *
+ * @param string $route
+ * @param string $action
+ * @param string $version
+ * @return void
+ */
+function routeError(string $route, string $action, string $version): void
 {
     header("Content-Type: application/json");
     http_response_code(405);
-    $errorArray[0]['Error'] = 'Method (' . $method . ') not available on this route with version number: ' . $version;
+    $errorArray[0]['Error'] = 'Action [ ' . $action . ' ] not available on route [ ' . $route . ' ] with version number [ ' . $version . ' ]';
+    echo json_encode($errorArray);
+    exit();
+}
+/**
+ * errorHandler
+ *
+ * @param integer $errCode
+ * @param string $errMsg
+ * @return void
+ */
+function errorHandler(int $errCode, string $errMsg): void
+{
+    header("Content-Type: application/json");
+    http_response_code($errCode);
+    $errorArray[0]['Error'] = $errMsg;
     echo json_encode($errorArray);
     exit();
 }
--- login-service/verify/1.0.1/help.php
+++ login-service/verify/1.0.1/help.php
@@ -4,11 +4,12 @@ echo "Version Number: 1.0.1" . PHP_EOL;
 echo "The username and password in this version are case sensitive" . PHP_EOL . PHP_EOL;
 echo "The following is the payload format (All fields are required): " . PHP_EOL . PHP_EOL;
 echo '{
-        "routing": "{Service Routing}",
-        "version": "{version.version.verison}",
-        "api-key": "{API-KEY}",
-        "client_id": "{Client ID}",
-        "username": "{USERNAME}",
-        "password": "{PASSWORD}"
+    "route": "{Service Routing}",
+    "action": "{Service Action}",
+    "version": "{version.version.verison}",
+    "api-key": "{API-KEY}",
+    "client_id": "{Client ID}",
+    "username": "{USERNAME}",
+    "password": "{PASSWORD}"
     }' . PHP_EOL . PHP_EOL;
 exit();
